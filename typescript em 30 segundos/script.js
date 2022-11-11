1d955da8d935624110102de1dda349ce6e99deb9
paulo-raoni <raoni@hotmail.com>
2022-11-10 21:11:47 -0300

    new fast project

---
+++ typescript em 30 segundos/script.js
@@ -0,0 +1,5 @@
+"use strict";
+const soma = (a, b) => {
+    return a + b;
+};
+console.log(soma(13, 22));
---
+++ typescript em 30 segundos/script.ts
@@ -0,0 +1,4 @@
+const soma = (a: number, b: number) => {
+    return a + b;
+}
+console.log(soma(13, 22));
